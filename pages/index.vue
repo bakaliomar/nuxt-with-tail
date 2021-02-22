3fbf4fa9b4da81bc3bfa29d7f3c0903530c7208c
bakaliomar <elbakaliomar9@gmail.com>
2021-02-22 01:16:03 +0100

    finish the footer section

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,58 @@
 <template lang="pug">
-  div
+  div.bg-black
     Navigation.top-0.inset-x-0.fixed
     SliderCard
     .h-20.mt-2.w-full.bg-gray-black.text-sm.flex.flex-col.items-center
       p.ml-2.mt-1.text-gray-400 We have multiple stream types for each anime, so try what works best for you.
       svg.h-8.w-8.text-gray-400.px-2.pt-2(xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor")
         path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7")
-    .mt-3.pb-px.border-b-2.border-gray-black.flex
+    .mt-3.border-b.border-gray-700.flex
       a.px-2.py-2.text-white.text-sm.bg-gray-700(href="#") New Eps
-      a.px-2.py-2.ml-px.text-custom-blue.text-sm.bg-gray-black(href="#") Updates
-      a.px-2.py-2.ml-px.text-custom-blue.text-sm.bg-gray-black(href="#") Popular
-      a.px-2.py-2.ml-px.text-custom-blue.text-sm.bg-gray-black(href="#") Recent
-      a.px-2.py-2.ml-px.text-custom-blue.text-sm.bg-gray-black(href="#") Movie
-    .mt-2.flex
-      .h-60.w-32.flex.flex-col.bg-gray-black
-        .card-image.h-44.relative
-          img.h-full.w-full.object-cover.object-center(src="~/assets/images/day.jpg")
-          .py-px.px-1.absolute.top-0.right-0.flex.items-center.bg-black.bg-opacity-50
-            svg.w-4.h-4.text-yellow-300(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor")
-              path(d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z")
-            span.text-white.text-xs.leading-4.ml-1 8.6
-          .py-px.px-1.absolute.bottom-0.right-0.text-white.text-xs.bg-black.bg-opacity-50 10 hours ago
-        .card-body.mt-2.text-center
-          a.text-custom-blue.text-xs(href="#")
-            h3.leading-tight Dragon Quest: Dai no Daibouken (2020)
-          p.mt-2.text-white.text-xs EP 20
-
+      a.mb-px.px-2.py-2.ml-px.text-custom-blue.text-sm.bg-gray-black(href="#") Updates
+      a.mb-px.px-2.py-2.ml-px.text-custom-blue.text-sm.bg-gray-black(href="#") Popular
+      a.mb-px.px-2.py-2.ml-px.text-custom-blue.text-sm.bg-gray-black(href="#") Recent
+      a.mb-px.px-2.py-2.ml-px.text-custom-blue.text-sm.bg-gray-black(href="#") Movie
+    .flex.flex-wrap.bg-black.h-full
+      Card(v-for="i in 24" :key="i")
+    .mt-8.px-12.mx-auto.h-9.w-52.flex.flex-wrap.justify-center.items-center.bg-gray-800.border.border-gray-700.rounded-sm
+      svg.h-6.w-6.text-custom-blue(xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor")
+        path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7")
+      span.text-custom-blue Load more
+    .h-9.mt-6.-mx-2.flex.justify-between.items-center.bg-gray-black.border-b.border-gray-900
+      .ml-2.flex.items.center
+        span.text-gray-400.text-sm Donate
+        svg.ml-2.h-5.w-5(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100")
+          circle(cx="13" cy="29" r="2" fill="#ee3e54")
+          circle(cx="77" cy="13" r="1" fill="#f1bc19")
+          circle(cx="50" cy="50" r="37" fill="#fce0a2")
+          circle(cx="83" cy="15" r="4" fill="#f1bc19")
+          circle(cx="87" cy="24" r="2" fill="#ee3e54")
+          circle(cx="81" cy="76" r="2" fill="#fbcd59")
+          circle(cx="15" cy="63" r="4" fill="#fbcd59")
+          circle(cx="25" cy="87" r="2" fill="#ee3e54")
+          circle(cx="19.5" cy="54.5" r="2.5" fill="#fff")
+          circle(cx="21" cy="67" r="1" fill="#f1bc19")
+          circle(cx="80" cy="34" r="1" fill="#fff")
+          circle(cx="50" cy="50" r="24.25" fill="#ef8f70")
+          path(fill="#472b29" d="M50 74.95c-13.758 0-24.95-11.192-24.95-24.95S36.242 25.05 50 25.05 74.95 36.242 74.95 50 63.758 74.95 50 74.95zm0-48.5c-12.985 0-23.55 10.564-23.55 23.55S37.015 73.55 50 73.55 73.55 62.986 73.55 50 62.985 26.45 50 26.45z")
+          path(fill="#472b29" d="M70.135 46.635a.5.5 0 01-.49-.406 19.782 19.782 0 00-2.917-7.196.5.5 0 01.836-.548 20.768 20.768 0 013.063 7.558.5.5 0 01-.492.592zM50 71c-11.579 0-21-9.421-21-21s9.421-21 21-21a20.878 20.878 0 0114.688 5.992.5.5 0 01-.699.714A19.89 19.89 0 0050 30c-11.028 0-20 8.972-20 20s8.972 20 20 20 20-8.972 20-20a.5.5 0 011 0c0 11.579-9.421 21-21 21z")
+          g
+            path(fill="#472b29" d="M41 40h4v19h-4z")
+            circle(cx="54.5" cy="47.5" r="7.5" fill="#fff")
+            path(fill="#472b29" d="M61.592 45.304a.249.249 0 01-.236-.168 7.22 7.22 0 00-2.558-3.475.25.25 0 01.297-.403 7.721 7.721 0 012.733 3.715.25.25 0 01-.236.331zM54.5 55.25c-4.273 0-7.75-3.477-7.75-7.75s3.477-7.75 7.75-7.75c.866 0 1.718.142 2.53.422a.25.25 0 01-.164.473 7.238 7.238 0 00-2.366-.395c-3.998 0-7.25 3.252-7.25 7.25s3.252 7.25 7.25 7.25 7.25-3.252 7.25-7.25a.25.25 0 01.5 0c0 4.273-3.477 7.75-7.75 7.75z")
+        svg.ml-2.w-5.h-5.text-yellow-500(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" fill="currentColor")
+          path(d="M24 4C12.972 4 4 12.972 4 24s8.972 20 20 20 20-8.972 20-20S35.028 4 24 4zm-.5 7a1.5 1.5 0 011.5 1.5V14h.5c3.033 0 5.5 2.467 5.5 5.5a5.46 5.46 0 01-1.082 3.262A6.003 6.003 0 0133 28c0 3.309-2.691 6-6 6h-2v1.5a1.5 1.5 0 11-3 0V34h-3.5a1.5 1.5 0 01-1.5-1.5v-17a1.5 1.5 0 011.5-1.5H22v-1.5a1.5 1.5 0 011.5-1.5zM20 17v5h5.5c1.378 0 2.5-1.122 2.5-2.5S26.878 17 25.5 17H20zm0 8v6h7c1.654 0 3-1.346 3-3s-1.346-3-3-3h-7z")
+      .mr-2.flex.items-center
+        span.text-gray-400.text-sm Chat
+        .ml-2.my-auto.w-8.h-4.flex.items-center.rounded-full.bg-gray-600
+          .m-0.h-full.bg-gray-400.rounded-full.outline-none(class="w-1/2")
+        svg.ml-2.w-5.h-5.text-custom-blue(xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor")
+          path(stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z")
+        svg.ml-2.w-5.h-5.text-custom-blue(xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" )
+          path(d="M25.855 5.574c-.94.418-1.953.7-3.015.828a5.279 5.279 0 002.308-2.906 10.515 10.515 0 01-3.335 1.274 5.251 5.251 0 00-8.945 4.79 14.908 14.908 0 01-10.821-5.49 5.23 5.23 0 00-.711 2.645c0 1.82.926 3.426 2.336 4.367a5.205 5.205 0 01-2.379-.656v.066a5.255 5.255 0 004.21 5.149 5.204 5.204 0 01-1.382.183c-.34 0-.668-.031-.988-.094a5.252 5.252 0 004.902 3.645 10.508 10.508 0 01-6.52 2.246c-.425 0-.84-.023-1.25-.07a14.858 14.858 0 008.047 2.355c9.657 0 14.938-8 14.938-14.933 0-.23-.004-.457-.016-.684a10.501 10.501 0 002.621-2.715")
+    .h-24.-mx-2.pb-4.text-center.bg-gray-black
+      h4.pt-2.text-xs.text-gray-400.font-medium AniMixPlay - Watch HD Anime Ad-Free ©2020 All Rights Reserved.
+      p.mt-2.text-xs.font-thin.leading-3.text-gray-600 Disclaimer: This site does not store any files on its server. All contents are provided by non-affiliated third parties.
 
 </template>
 
@@ -33,10 +60,12 @@
 import { Component, Vue } from "vue-property-decorator";
 import Navigation from '~/components/navbar/Navigation.vue';
 import SliderCard from '~/components/SliderCard.vue';
+import Card from "~/components/Card.vue"
 @Component ({
   components: {
     Navigation,
-    SliderCard
+    SliderCard,
+    Card
   }
 })
 export default class Index extends Vue{
