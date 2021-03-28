f9ba4e0596c5e0dd2d08347770ddd2b553f839ea
bakaliomar <elbakaliomar9@gmail.com>
2021-03-28 20:16:32 +0100

    refactor the code preparing for linking it with the backend

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
-  div.w-full
     .px-2(class="lg:w-8/12 xl:w-9/12")
       SliderCard
       Discord
       Tabs
-      Cards
+      div
+        .flex.flex-wrap.bg-black.h-full.-ml-2.mt-2
+          Card(v-for="i in 24" :key="i")
+        LoadMore
 </template>
 
 <script lang="ts">
